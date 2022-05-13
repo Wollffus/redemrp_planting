de7956d63ae188ab1d512ce0f95c91cfcd4273ae
Wollffus <58875783+Wollffus@users.noreply.github.com>
2022-05-13 16:52:22 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,8 @@
+fx_version 'bodacious'
+
 game 'rdr3'
-fx_version 'adamant'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
 
 client_scripts {
     'config.lua',
